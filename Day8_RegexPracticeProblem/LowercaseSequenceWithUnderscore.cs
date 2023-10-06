bfdb721e81cacdd641333ebeb19a158bffde4570
Rohan <rohandute1@gmail.com>
2023-10-06 12:50:40 +0530

    [rohan] Successfully added program to find lowercase letter sequences with underscores

---
+++ Day8_RegexPracticeProblem/LowercaseSequenceWithUnderscore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8_RegexPracticeProblem
+{
+    public class LowercaseSequenceWithUnderscore
+    {
+        public static void ValidateSequenceOfLowercase()
+        {
+            
+            string[] inputs = { "abc_nv", "bvh_dVsn_cn", "nkn_jd", "_dcn" };
+
+            string pattern = "^[a-z]+_[a-z]+$";
+
+            foreach (string input in inputs)
+            {
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Console.WriteLine("{0} is Valid", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Invalid", input);
+                }
+                    
+            }
+
+        }
+    }
+}
--- Day8_RegexPracticeProblem/Program.cs
+++ Day8_RegexPracticeProblem/Program.cs
@@ -12,13 +12,18 @@ namespace Day8_RegexPracticeProblem
         {
             Console.WriteLine("Welcome to regex practice problems.");
             Console.WriteLine("Please select program form list:");
-            Console.WriteLine("1.Program that matches a string that has an ‘a’ followed by two to three 'b'.");
+            Console.WriteLine("1.Program that matches a string that has an ‘a’ followed by two to three 'b'.\n" +
+                "2.Program to find sequences of lowercase letters joined by an underscore.");
             int choose = Convert.ToInt32(Console.ReadLine());
 
             switch (choose)
             {
                 case 1:
                     ValidateAwith2to3B.StringMatcher();
+                    break;
+
+                case 2:
+                    LowercaseSequenceWithUnderscore.ValidateSequenceOfLowercase();
                     break;
             }
 
