b12c53ac145e2c2b732bbdf13b95d439f008f972
Rohan <rohandute1@gmail.com>
2023-10-06 13:24:21 +0530

    [rohan] Successfully added functionality to fetch HTML tags from given string

---
+++ Day8_RegexPracticeProblem/HtmlTagsFetcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8_RegexPracticeProblem
+{
+    public class HtmlTagsFetcher
+    {
+        public static void FetchHtmlTags()
+        {
+            string input = "<p>The <code>Regex</code> is a compiled representation of a regular expression.</p><>";
+            string pattern = "<[^>]*>";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
--- Day8_RegexPracticeProblem/Program.cs
+++ Day8_RegexPracticeProblem/Program.cs
@@ -13,7 +13,8 @@ namespace Day8_RegexPracticeProblem
             Console.WriteLine("Welcome to regex practice problems.");
             Console.WriteLine("Please select program form list:");
             Console.WriteLine("1.Program that matches a string that has an ‘a’ followed by two to three 'b'.\n" +
-                "2.Program to find sequences of lowercase letters joined by an underscore.");
+                "2.Program to find sequences of lowercase letters joined by an underscore.\n" +
+                "3.Program to fetch all HTML tags from a given string.");
             int choose = Convert.ToInt32(Console.ReadLine());
 
             switch (choose)
@@ -24,6 +25,10 @@ namespace Day8_RegexPracticeProblem
 
                 case 2:
                     LowercaseSequenceWithUnderscore.ValidateSequenceOfLowercase();
+                    break;
+
+                case 3:
+                    HtmlTagsFetcher.FetchHtmlTags();
                     break;
             }
 
