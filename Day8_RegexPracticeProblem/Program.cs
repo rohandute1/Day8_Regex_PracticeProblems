5ba1692249e3e2483c661784b29e128d9269c9a4
Rohan <rohandute1@gmail.com>
2023-10-06 11:42:06 +0530

    [rohan] Successfully added initial welcome message

---
+++ Day8_RegexPracticeProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_RegexPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to regex practice problems.");
+
+            Console.ReadLine();
+        }
+    }
+}
