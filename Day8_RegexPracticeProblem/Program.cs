95eaecfc34f2539bca608c87774a899b15c4a9e0
Rohan <rohandute1@gmail.com>
2023-10-06 12:13:29 +0530

    [rohan] Added string matching functionality using regular expressions

--- Day8_RegexPracticeProblem/Program.cs
+++ Day8_RegexPracticeProblem/Program.cs
@@ -11,6 +11,16 @@ namespace Day8_RegexPracticeProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to regex practice problems.");
+            Console.WriteLine("Please select program form list:");
+            Console.WriteLine("1.Program that matches a string that has an ‘a’ followed by two to three 'b'.");
+            int choose = Convert.ToInt32(Console.ReadLine());
+
+            switch (choose)
+            {
+                case 1:
+                    ValidateAwith2to3B.StringMatcher();
+                    break;
+            }
 
             Console.ReadLine();
         }
---
+++ Day8_RegexPracticeProblem/ValidateAwith2to3B.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8_RegexPracticeProblem
+{
+    public class ValidateAwith2to3B
+    {
+        public static void StringMatcher()
+        {
+
+            Console.WriteLine("Please select from below options :-");
+            Console.WriteLine("1.Inbuilt input with array\n2.Provide user input");
+            int select = Convert.ToInt32(Console.ReadLine());
+            switch(select)
+            {
+                case 1:
+                    string[] inputs = { "aaa", "aabbb", "abbb", "bbb", "a", "abb", "abbbbb" };
+                    string pattern = "^a[b]{2,3}$";
+
+                    foreach(string input in inputs)
+                    {
+                        if (Regex.IsMatch(input, pattern))
+                        {
+                            Console.WriteLine("{0} is a valid input.",input);
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} is a invalid input.",input);
+                        }
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("Please enter string which you want to check :-");
+                    string str = Console.ReadLine();
+                    string patern = "^a[b]{2,3}$";
+                    if(Regex.IsMatch(str, patern))
+                    {
+                        Console.WriteLine("{0} is a valid input.", str);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} is a invalid input.", str);
+                    }
+                    break;
+            }
+        }
+    }
+}
